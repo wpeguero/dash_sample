fa60654613771393a8ed009e0e274daec2563329
wpeguero <wpeguerorosario@outlook.com>
2024-04-26 10:41:26 -0400

    Finished The Dashboard Example

--- dashboard2.py
+++ dashboard2.py
@@ -6,7 +6,7 @@ import json
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-df = pd.read_csv("ObesityDataSet_raw_and_data_sinthetic.csv")
+df = pd.read_csv("data/ObesityDataSet_raw_and_data_sinthetic.csv")
 df = df.sample(n=100, random_state=42)
 df['count'] = 1
 
@@ -35,7 +35,8 @@ def color_calc(row):
         return 'purple'
 
 style = {
-        'display': 'inline-block'
+        'display': 'inline-block',
+        'width': '49vw',
         }
 df['colors'] = df.apply(color_calc, axis=1)
 gender_pie = px.pie(df, names='Gender', title='Gender')
@@ -44,32 +45,36 @@ gender_pie.update_layout(title_x=0.5, clickmode='event+select')
 fig = px.histogram(df, x='NObeyesdad', y='count', title='Alcohol Frequency', labels={'NObeyesdad': 'Level of Obesity', 'count':'People'}, color='NObeyesdad')
 fig.update_layout(title_x=0.5)
 
-fig2 = px.histogram(df, x='NObeyesdad', y='FAF', color='NObeyesdad', labels={'NObeyesdad': 'Level of Obesity', 'FAF':'Physical Activity'})
+fig2 = px.histogram(df, x='FAVC', color='FAVC', labels={'FAVC': 'High Caloric Food Intake'}, title='High Caloric Food Compsumption')
 
-fig3 = px.density_heatmap(df,x='MTRANS', y='TUE')
+fig3 = px.histogram(df, x='family_history_with_overweight', color='family_history_with_overweight', labels={'family_history_with_overweight': 'Overweight Family History'}, title='Family History')
+fig3.update_layout(title_x=0.5)
 app = Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div([
     html.H1(children='Sample Dashboard From Green Key Resources', style={'textAlign': 'center', 'font':40}),
-        html.Div([
-        dcc.Graph(id='filter',figure=gender_pie, style=style),    
-        dcc.Graph(id='click-filter', figure=fig, style=style),
-        #html.Pre(id='click-filter')
+        html.Div([   
+            dcc.Graph(id='filter',figure=gender_pie, style=style),
+            dcc.Graph(id='click-filter3', figure=fig3, style=style),
+            #html.Pre(id='click-filter')
         ]),
         html.Div([
             dcc.Graph(id='click-filter2', figure=fig2, style=style),
-            dcc.Graph(figure=fig3, style=style),
+            dcc.Graph(id='click-filter', figure=fig, style=style),
         ]),
         html.Div([
             html.H4(children='Detail', style={'textAlign': 'center'})
         ]),
-        dash_table.DataTable(df.to_dict('records'), page_size=10, style_cell={'textAlign': 'center'})
-    ], style=style)
+        html.Div([
+            dash_table.DataTable(df.to_dict('records'), page_size=10, style_cell={'textAlign': 'center'})
+        ]),
+        
+    ])
 
 @callback(
     Output('click-filter', 'figure'),
     Output('click-filter2', 'figure'),
-    #Output('click-filter3', 'figure'),
+    Output('click-filter3', 'figure'),
     Input('filter', 'clickData')
 )
 def filter(clickData):
@@ -77,17 +82,20 @@ def filter(clickData):
     cdata = json.dumps(clickData, indent=2)
     if cdata == 'null':
         fig = px.histogram(df, x='NObeyesdad', y='count', title='Alcohol Frequency', labels={'NObeyesdad': 'Level of Obesity', 'count':'People'}, color='NObeyesdad')
-        fig2 = px.histogram(df, x='NObeyesdad', y='FAF', color='NObeyesdad', labels={'NObeyesdad': 'Level of Obesity', 'FAF':'Physical Activity'})
+        fig2 = px.histogram(df, x='FAVC', color='FAVC', labels={'FAVC': 'High Caloric Food Intake'}, title='High Caloric Food Compsumption')
+        fig3 = px.histogram(df, x='family_history_with_overweight', color='family_history_with_overweight', labels={'family_history_with_overweight': 'Overweight Family History'}, title='Family History')
     else:
         cdata = json.loads(cdata)
         value = cdata['points'][0]['label']
         filtered_df = df[df['Gender'] == value]
-        fig = px.histogram(filtered_df, x='NObeyesdad', y='count', title='Alcohol Frequency', labels={'NObeyesdad': 'Level of Obesity', 'count':'People'}, color='NObeyesdad')
-        fig2 = px.histogram(filtered_df, x='NObeyesdad', y='FAF', color='NObeyesdad', labels={'NObeyesdad': 'Level of Obesity', 'FAF':'Physical Activity'})
-    fig.update_layout(title_x=0.5)
-    return fig, fig2
+        fig = px.histogram(filtered_df.sort_values(by='NObeyesdad'), x='NObeyesdad', y='count', title='Alcohol Frequency', labels={'NObeyesdad': 'Level of Obesity', 'count':'People'}, color='NObeyesdad')
+        fig2 = px.histogram(filtered_df, x='FAVC', color='FAVC', labels={'FAVC': 'High Caloric Food Intake'}, title='High Caloric Food Compsumption')
+        fig3 = px.histogram(filtered_df, x='family_history_with_overweight', color='family_history_with_overweight', labels={'family_history_with_overweight': 'Overweight Family History'}, title='Family History')
+    for f in [fig, fig2, fig3]:
+        f.update_layout({'title_x':0.5})
+    return fig, fig2, fig3
 
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)
